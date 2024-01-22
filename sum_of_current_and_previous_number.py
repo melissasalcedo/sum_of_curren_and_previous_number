34e4a8938ab70a6a93065dd7d45714be746e5af9
melissasalcedo <salcedomelissa189@gmil.com>
2024-01-22 14:25:09 +0800

    Finished

--- sum_of_current_and_previous_number.py
+++ sum_of_current_and_previous_number.py
@@ -1,12 +1,12 @@
-print("Printing current and previous number and their sum in a range(10)")
-previous_num = 0
+# Print the sum of the current number and the previous number
 
-# loop from 1 to 10
-for i in range(1, 11):
-    x_sum = previous_num + i
-    print("Current Number", i, "Previous Number ", previous_num, " Sum: ", x_sum)
-    # modify previous number
-    # set it to the current number
-    previous_num = i
+previous_number = 0
+# Current number
+for i in range(1,11):
+    # Adding the current and previous
+    number_sum = previous_number + i
+    print("The current number:", i, "The previous number:", previous_number, "Sum:", number_sum)
+    previous_number = i 
+
 
     
